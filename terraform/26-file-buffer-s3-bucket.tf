90f4cdd91c3a536e3765dbefa1f9df58168e0000
Adrian Clay <adrian@madetech.com>
2021-07-19 11:44:32 +0100

    Remove double -

--- terraform/26-file-buffer-s3-bucket.tf
+++ terraform/26-file-buffer-s3-bucket.tf
@@ -4,6 +4,6 @@ module "file-buffer" {
   project = ""
   environment = var.environment
   bucket_identifier = "file-buffers"
-  identifier_prefix = "file-sync-infra-"
+  identifier_prefix = "file-sync-infra"
   role_arns_to_share_access_with = []
 }
