1f187e44caab8c1cc33de6acd167aded2229b915
elcorbs <emma@madetech.com>
2021-07-19 15:47:33 +0100

    Correct assembly and namespace name.
    Makefile command to test hello world function

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ apply:
 deploy:
 	cd hello_world/src/hello_world && dotnet lambda package hello_world
 	aws-vault exec hackney-dev-scratch -- aws s3 cp hello_world/src/hello_world/hello_world.zip s3://file-sync-infra-build-artifacts/hello_world.zip
+
+test_hello_world:
+	aws-vault exec hackney-dev-scratch -- aws lambda invoke --function-name hello-world --payload "hello" --cli-binary-format raw-in-base64-out ./response.txt
--- terraform/27-hello-world.tf
+++ terraform/27-hello-world.tf
@@ -19,14 +19,9 @@ resource "aws_iam_role" "hello_world" {
 
 resource "aws_lambda_function" "hello_world" {
   role             = aws_iam_role.hello_world.arn
-  handler          = "hello-world::hello-world.Function::FunctionHandler"
+  handler          = "hello_world::hello_world.Function::FunctionHandler"
   runtime          = "dotnetcore3.1"
   function_name    = "hello-world"
   s3_bucket        = module.lambda_build_artifacts.bucket_id
   s3_key           = "hello_world.zip"
-
-  environment {
-    variables = {
-    }
-  }
 }
