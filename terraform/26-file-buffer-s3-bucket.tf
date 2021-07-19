3de1329ecf7af79682ec31a19c63520bba827e8b
Adrian Clay <adrian@madetech.com>
2021-07-19 14:20:56 +0100

    Add buid artifacts S3 bucket

--- terraform/26-file-buffer-s3-bucket.tf
+++ terraform/26-file-buffer-s3-bucket.tf
@@ -1,9 +1,19 @@
-module "file-buffer" {
+module "file_buffer" {
   source = "../modules/s3-bucket"
   tags = {}
   project = ""
   environment = var.environment
   bucket_identifier = "file-buffers"
+  identifier_prefix = "file-sync-infra"
+  role_arns_to_share_access_with = []
+}
+
+module "lambda_build_artifacts" {
+  source = "../modules/s3-bucket"
+  tags = {}
+  project = ""
+  environment = var.environment
+  bucket_identifier = "build-artifacts"
   identifier_prefix = "file-sync-infra"
   role_arns_to_share_access_with = []
 }
